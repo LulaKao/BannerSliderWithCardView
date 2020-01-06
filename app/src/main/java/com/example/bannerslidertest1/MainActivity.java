4be934069edfc21070cfb24178d5849eb781ce6d
LulaKao <windandsprings@gmail.com>
2020-01-06 11:37:56 +0800

    調整 layout

--- app/src/main/java/com/example/bannerslidertest1/MainActivity.java
+++ app/src/main/java/com/example/bannerslidertest1/MainActivity.java
@@ -22,50 +22,60 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // 初始化元件
+        viewPager = findViewById(R.id.viewPager);
+
+        // 設置 banner 的圖片
         models = new ArrayList<>();
-        models.add(new Model(R.drawable.aa));
+        models.add(new Model(R.drawable.bb));
+        models.add(new Model(R.drawable.bn));
         models.add(new Model(R.drawable.bn2));
         models.add(new Model(R.drawable.video));
 
+        // 設置 Adapter
         adapter = new Adapter(models,this);
-
-        viewPager = findViewById(R.id.viewPager);
         viewPager.setAdapter(adapter);
-        viewPager.setPadding(130,0,130,0);
 
-        Integer[] colors_temp = {
-                getResources().getColor(R.color.color1),
-                getResources().getColor(R.color.color2),
-                getResources().getColor(R.color.color3),
-        };
+        // 設置 banner 彼此之間的間距
+        viewPager.setPadding(60,0,60,0);
 
-        colors = colors_temp;
-
-        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-                if(position < (adapter.getCount() - 1) && position < (colors.length - 1)){
-                    viewPager.setBackgroundColor(
-                            (Integer)argbEvaluator.evaluate(
-                                    positionOffset,
-                                    colors[position],
-                                    colors[position + 1]
-                            )
-                    );
-                }else {
-                    viewPager.setBackgroundColor(colors[colors.length - 1]);
-                }
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-
-            }
-        });
+//        //--- 設置背景色 START ---//
+//        Integer[] colors_temp = {
+//                getResources().getColor(R.color.color1),
+//                getResources().getColor(R.color.color2),
+//                getResources().getColor(R.color.color3),
+//        };
+//
+//        colors = colors_temp;
+//        //--- 設置背景色 END ---//
+//
+//        //--- 讓背景色隨著換頁而改變 START ---//
+//        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+//            @Override
+//            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+//                if(position < (adapter.getCount() - 1) && position < (colors.length - 1)){
+//                    viewPager.setBackgroundColor(
+//                            (Integer)argbEvaluator.evaluate(
+//                                    positionOffset,
+//                                    colors[position],
+//                                    colors[position + 1]
+//                            )
+//                    );
+//                }else {
+//                    viewPager.setBackgroundColor(colors[colors.length - 1]);
+//                }
+//            }
+//
+//            @Override
+//            public void onPageSelected(int position) {
+//
+//            }
+//
+//            @Override
+//            public void onPageScrollStateChanged(int state) {
+//
+//            }
+//        });
+//        //--- 讓背景色隨著換頁而改變 END ---//
     }
 }
